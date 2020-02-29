a143758fc07d5ef7e167ac5bd944bab6bc59bdbf
aalok <a.m.sathe@sms.ed.ac.uk>
2020-02-29 02:53:07 +0000

    some stuff

--- simulate.py
+++ simulate.py
@@ -13,10 +13,8 @@ def main():
     '''
     print('='*64)
 
-    speakers = []
-    listeners = []
-
-    lexdict = [({0,1}, 'zeroone'), ({0, 2}, 'zerotwo'), ({1, 2}, 'onetwo')]
+    lexdict = [({0, 1}, 's01'), ({0, 1, 2}, 's012'), ({1, 2, 3}, 's123'),
+               ({1, 2}, 's12')]
 
     entries = []
     for meanings, word in lexdict:
@@ -29,44 +27,46 @@ def main():
     print('='*32)
     print()
 
-    # a0 = Agent(0, lexicon)
-    # tgt = 0
-    # print('Emission probabilities of s0 to speak target={}:'.format(tgt))
-    # print(a0.speakdist(tgt))
-    #
-    # # l0 = Agent(0, lexicon=lexicon)
-    # word = 'word2'
-    # print('Listener probabilities of l0 on hearing word={}:'.format(word))
-    # print(a0.listendist(word))
-
-    for level in range(100):
+    generations = 500
+    for level in range(generations):
         print('-'*64)
         print('Speaker level:\t{}'.format(level))
+        # s = Agent(level, lexicon, noise=lambda _: 1)
         s = Agent(level, lexicon)
         print('Listener level:\t{}'.format(level))
         l = Agent(level, lexicon)
 
+        def fmtfloatdict(d):
+            return '  '.join(['"{}" : {: >3.4f}'.format(k,v) for k,v in d.items()])
+
         for tgt in lexicon.possible_referents():
             print('Speaker {} goal:\t{}'.format(level, tgt))
-            print('\t', s.speakdist(tgt))
+            print('\t', fmtfloatdict(s.speakdist(tgt)))
 
         print()
         for word in lexicon.possible_words():
             print('Listener {} hears:\t{}'.format(level, word))
-            print('\t', l.listendist(word))
+            print('\t', fmtfloatdict(l.listendist(word)))
 
-        # if level < 90: continue
+        if level < generations-2: continue
         print()
         trials = 1000
-        print('Some monte-carlo over {} runs'.format(trials))
-        outcomes = defaultdict(list)
+        print('Monte-carlo over {} runs'.format(trials))
+        outcomes = defaultdict(lambda: defaultdict(lambda: 0))
         for trial in range(trials):
             for r in lexicon.possible_referents():
-                outcomes[r] += ['tgt: {}, act: {}'.format(r, l.listen(s.speak(r)))]
+                infr = l.listen(s.speak(r))
+                outcomes[r][infr] += 1
+
+        fmt = '{: >5} '*4
+        print(' ', fmt.format(*lexicon.possible_referents()), sep='\t')
+        print()
         for r in lexicon.possible_referents():
-            print('\t', Counter(outcomes[r]))
+            for r_ in lexicon.possible_referents(): outcomes[r][r_]
+            print(r, fmt.format(*[v for k, v in sorted(outcomes[r].items())]), sep='\t')
 
     print('='*64)
+
 
 if __name__ == '__main__':
     parser = ArgumentParser()
